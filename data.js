e3f589c3f3a013dd5d0a9b99d69952f1c6661443
Vodkamartini <martin.marklund@hotmail.se>
2020-02-29 14:57:57 +0100

    New version of Cygni test

--- data.js
+++ data.js
@@ -4,9 +4,11 @@
 // Secret:
 // 3c60e68733933248
 
+
+var api_key = '240a07e3f563e8510c857490af4e846f';
 // Tags can be changed to serach for other images. Several tags can be used, separated by a ','.
-var tags = 'space, planets'
-var url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=240a07e3f563e8510c857490af4e846f&tags=${tags}&safe_search=3&format=json&nojsoncallback=1`
+var tags = 'bird'
+var url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${api_key}&tags=${tags}&safe_search=3&per_page=${250}&format=json&nojsoncallback=1`
 
 export async function getData() {
     
--- images.js
+++ images.js
@@ -1,7 +1,10 @@
-// Import 
+// Import fetched data
 import {getData} from './data.js';
 
+// Arrays with all images and alternative texts
 const IMAGES = [];
+const ALTS = [];
+const GALLERY_SIZE = 240;
 
 // Wait for 'window' to load before running the rest of the script
 addEventListener('load', main);
@@ -9,46 +12,62 @@ addEventListener('load', main);
 async function main() {
     removeEventListener('load', main);  // 'window' loaded, no need to listen for it.
 
+    createGallery(GALLERY_SIZE);
+
     await getData().then((data) => {
         createImageFromUrl(data);
     });
 
-    createGallery();
 
+    fillGallery();
 }
 
 // Push image URL:s to IMAGES according to Flickr's Photo Source URLS.
 function createImageFromUrl(data) {
+    //console.log(data)
     let photos = data.photos.photo;
     for (let i = 0; i < photos.length; ++i) {
         IMAGES.push(`https://farm${photos[i].farm}.staticflickr.com/${photos[i].server}/${photos[i].id}_${photos[i].secret}.jpg`);
+        ALTS.push(photos[i].title);
     }
 }
 
-function createGallery() {
+// Create gallery template with loading images.
+function createGallery(numberOfImages) {
 
     var gallery = document.getElementById("gallery");
 
-    for(let i = 0; i < IMAGES.length; ++i) {
-        var galleryImg = document.createElement("IMG");
-        galleryImg.setAttribute("src", IMAGES[i]);
-        galleryImg.classList.add("gallery-img");
-        gallery.appendChild(galleryImg);
+    for(let i = 0; i < numberOfImages; ++i) {
+        var image = new Image();
+        image.classList.add("loading-img");
+        image.src = "images/loader_ripple.gif";
+        image.alt = "The image is either loading or cannot be found."
+        gallery.appendChild(image);
     }
-    
 }
 
+// Replace loading icons with images when fully loaded.
+function fillGallery() {
 
-/*
-getData().then((myJson) => {
-
-    
-
-    var photos = myJson.photos.photo;
-    for(let i = 0; i < photos.length; ++i) {
-        //console.log(photos[i]);
-        let url = `https://farm${photos[i].farm}.staticflickr.com/${photos[i].server}/${photos[i].id}_${photos[i].secret}.jpg`
-        images[i] = url;
+    var galleryImgs = document.images;  // Get all <img>-tags on page.
+    for(let i = 0; i < GALLERY_SIZE; i++)
+    {
+        // Create a new image that will be used to load the correct src.
+        var loadingImage = new Image();
+        loadingImage.onload = function() {  // Only when image is fully loaded, the loading gif will be replaced.
+            galleryImgs[i].classList.replace("loading-img", "gallery-img");
+            galleryImgs[i].src = this.src;        
+            galleryImgs[i].alt = ALTS[i];
+        };
+        if(i == 2)
+            loadingImage.src ="afosihasof";
+        else
+            loadingImage.src = IMAGES[i];
+        
+        loadingImage.onerror = function() { // If the image could not be found (broken link etc.) use local error image.
+            
+            galleryImgs[i].alt="Failed to load image.";
+            galleryImgs[i].src="images/error.png";
+        };
     }
-});
-*/
+}
